0c750e8a15864490229470069820f3fb199425b2
clumsyninja196 <hendrabi@mail.gvsu.edu>
2020-07-09 11:55:25 -0400

    completed practice tracker sprint 1

--- ExcelHandler.m
+++ ExcelHandler.m
@@ -12,5 +12,19 @@ classdef ExcelHandler < handle
         function addData(obj,data)
             xlswrite(obj.filename,data);           
         end
+        
+        function value = getData(obj, name)
+            [~,~,data] = xlsread(obj.filename);
+            value = data{ismember(data(:,1), name),2};
+            
+            % Error checking
+            if isnan(value)
+                value = ' ';
+            end
+            
+            if isa(value,'double')
+                value = num2str(value);
+            end
+        end
     end
 end
